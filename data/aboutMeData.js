d8e55b9fa220917429578794c20267fc3516bc38
Drucilla <74247328+drucimimi@users.noreply.github.com>
2024-07-01 13:19:09 +0200

    Update aboutMeData.js

--- data/aboutMeData.js
+++ data/aboutMeData.js
@@ -3,6 +3,6 @@ import { v4 as uuidv4 } from 'uuid';
 export const aboutMeData = [
 	{
 		id: uuidv4(),
-		bio: 'Développeuse Full Stack depuis 2 ans chez Accenture, j'ai pu monter en compétences sur le développement web en général. Actuellement, je me spécialise sur le développement front d'applications web.'
+		bio: "Développeuse Full Stack depuis 2 ans chez Accenture, j'ai pu monter en compétences sur le développement web en général. Actuellement, je me spécialise sur le développement front d'applications web."
 	}
 ];
