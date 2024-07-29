2573522f8e49e4bb4c93be55c2247cef70569128
drucimimi <drucilladeroche@protonmail.com>
2024-07-29 21:39:31 +0200

    Test auth github

--- app/actions/projects.action.ts
+++ app/actions/projects.action.ts
@@ -1,7 +1,15 @@
 import { Octokit } from "@octokit/rest"
+import { createAppAuth } from "@octokit/auth-app";
 
 export default class Projects {
-    static octokit = new Octokit({ auth: process.env.GITHUB_TOKEN, timeZone: "Europe/Paris" })
+    //static octokit = new Octokit({ auth: process.env.GITHUB_TOKEN, timeZone: "Europe/Paris" })
+    static octokit = new Octokit({
+        authStrategy: createAppAuth,
+        auth: {
+          appId: 956783,
+          privateKey: process.env.PRIVATE_KEY,
+        },
+      })
     static async get(){
         try {
             const response : { viewer: { projectsV2: { nodes: [{id:number, title:string, shortDescription:string, closed:boolean}] } } } = await this.octokit.graphql(`{
