428e66ee0ccc3b23c6de38e0ef91b93435f9c034
drucimimi <drucilladeroche@protonmail.com>
2024-10-16 11:44:46 +0200

    Ajout de métadonnées

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,6 +12,34 @@ export const metadata: Metadata = {
   icons: {
     icon: '/images/favicon.ico',
   },
+  openGraph: {
+    title: 'Portfolio de Drucilla Deroche',
+    description: "Comprend ma présentation, mes projets, mes expériences professionnelles ainsi que mes compétences",
+    url: 'https://drucilla-deroche.vercel.app',
+    siteName: 'Portfolio de Drucilla Deroche',
+    images: [
+      {
+        url: 'https://drucilla-deroche.vercel.app/images/drucilla.jpg',
+        width: 800,
+        height: 600,
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Portfolio de Drucilla Deroche',
+    description: 'Comprend ma présentation, mes projets, mes expériences professionnelles ainsi que mes compétences',
+    images: ['https://drucilla-deroche.vercel.app/images/drucilla.jpg'],
+  },
+  robots: {
+    index: false,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: false,
+    },
+  },
 }
 
 export async function generateStaticParams() {
