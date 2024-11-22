011f2b29fe9943c8bbc28b1f3e58a6f5b23aec31
drucimimi <drucilladeroche@protonmail.com>
2024-11-22 17:41:58 +0100

    Intégration de Matomo

--- app/ui/_components/Provider.tsx
+++ app/ui/_components/Provider.tsx
@@ -1,5 +1,7 @@
+"use client"
 import { I18nProviderClient } from "@/locales/client"
-import { type ReactNode } from "react"
+import init from "@socialgouv/matomo-next"
+import { useEffect, type ReactNode } from "react"
 
 type ProviderProps = {
   locale: string
@@ -7,6 +9,11 @@ type ProviderProps = {
 }
 
 export function Provider({ locale, children }: ProviderProps) {
+  const MATOMO_URL = process.env.MATOMO_URL || "https://matomo.webapps24.eu/matomo.php";
+  const MATOMO_SITE_ID = process.env.MATOMO_SITE_ID || "4";
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
   return (
     <I18nProviderClient locale={locale}>
       {children}
