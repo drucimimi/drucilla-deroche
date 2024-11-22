ac79109e530499bd7c5500f874a6c6c07770cad0
drucimimi <drucilladeroche@protonmail.com>
2024-11-22 17:45:51 +0100

    Intégration Matomo #2

--- app/ui/_components/Provider.tsx
+++ app/ui/_components/Provider.tsx
@@ -9,7 +9,7 @@ type ProviderProps = {
 }
 
 export function Provider({ locale, children }: ProviderProps) {
-  const MATOMO_URL = process.env.MATOMO_URL || "https://matomo.webapps24.eu/matomo.php";
+  const MATOMO_URL = process.env.MATOMO_URL || "https://matomo.webapps24.eu";
   const MATOMO_SITE_ID = process.env.MATOMO_SITE_ID || "4";
   useEffect(() => {
     init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
